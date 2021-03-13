a9a46fad1c92f8c252e7dc2cf0347654c8608307
Unknown <carlos15erh@gmail.com>
2021-03-13 17:17:00 -0500

    code division in context.js

--- client/src/Context.js
+++ client/src/Context.js
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useReducer } from 'react';
 import { dataForApp, updatingPlayer } from './utils/Context';
-// import { dataForApp, updatingPlayer } from './utils/Context';
 const Context = createContext();
 
 let initialState = {
@@ -19,25 +18,10 @@ const reducer2 = (state, action) => {
       };
     case 'UPDATE_DATA':
       if (action.payload.updatePlayer.medail !== 'Sin Calibrar') {
-<<<<<<< HEAD
         let stateWithUpdatedPlayer = updatingPlayer(
           state,
           action.payload,
           'Calibrado'
-=======
-        let updatedPlayer = updatingPlayer(state, action.payload);
-
-        let indexInRanking = findPlayer(state.ranking, updatedPlayer['_id']);
-        if (indexInRanking === -1) {
-          state.ranking.push(updatedPlayer);
-        } else {
-          state.ranking[indexInRanking] = updatedPlayer;
-        }
-        sortPlayers(state.ranking);
-        let indexInAllPlayers = findPlayer(
-          state.allPlayers,
-          updatedPlayer['_id']
->>>>>>> master
         );
         return {
           ...stateWithUpdatedPlayer,
--- client/src/utils/Context/index.js
+++ client/src/utils/Context/index.js
@@ -1,101 +1,12 @@
-export function sortPlayers(players) {
-  return players.sort((a, b) => {
-    if (b.mmr === a.mmr) {
-      return b.kda - a.kda;
-    }
-    return b.mmr - a.mmr;
-  });
-}
-
-function setScoreOfPlayers(players, score) {
-  const playersWithScore = players.map((player) => {
-    return scoreOnePlayer(player, score);
-  });
-
-  return playersWithScore;
-}
-
-function scoreOnePlayer(player, score) {
-  let rolesScore = score.find((score) => player['_id'] === score.playerId)
-    .rolesScore;
-  return { ...player, rolesScore };
-}
-
-function orderedRoles(player, roles) {
-  let playerWithOrderedRoles = JSON.parse(JSON.stringify(player));
-  let orderedRoles = roles.map((rol) => {
-    let sortRol = playerWithOrderedRoles.rolesScore.find(
-      (rolScore) => rolScore.rol === rol['_id']
-    );
-    return { ...sortRol, rol: rol.name };
-  });
-  playerWithOrderedRoles.rolesScore = orderedRoles;
-  return playerWithOrderedRoles;
-}
-
-function calcKdaAndGames(rolesScore) {
-  let partidas = 0;
-  let obj = rolesScore.reduce(
-    (acc, cv) => {
-      partidas +=
-        cv.victories + cv.victoriesDouble + cv.defeats + cv.defeatsDouble;
-      return (acc = {
-        ...acc,
-        kills: acc.kills + cv.kills,
-        deaths: acc.deaths + cv.deaths,
-        assists: acc.assists + cv.assists,
-      });
-    },
-    { kills: 0, deaths: 0, assists: 0 }
-  );
-
-  let kda = ((obj.kills + obj.assists) / obj.deaths).toFixed(2);
-  kda = kda.length === 4 ? kda : kda.length === 1 ? `${kda}.00` : `${kda}0`;
-  return { partidas, kda };
-}
-
-function isCalibrated(player) {
-  if (player.medail !== 'Sin Calibrar') {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function setMedail(player, medails) {
-  let newPlayer = Object.assign({}, player);
-  let calibrated = isCalibrated(player);
-  if (calibrated) {
-    newPlayer.medail = medails.find(
-      (medail) => newPlayer.medail === medail['_id']
-    ).name;
-  }
-  return newPlayer;
-}
-
-function setKDAAndMedail(players, medails, roles) {
-  const results = players.map((player) => {
-    let playerWithOrderedRoles = orderedRoles(player, roles);
-    let { partidas, kda } = calcKdaAndGames(playerWithOrderedRoles.rolesScore);
-
-    let playerWithMedail = setMedail(playerWithOrderedRoles, medails);
-
-    return {
-      ...playerWithMedail,
-      kda,
-      partidas,
-    };
-  });
-  return results;
-}
-
-function filteringPlayers(players) {
-  const filteredPlayers = players.filter(
-    (player) => player.estado === true && player.medail !== 'Sin Calibrar'
-  );
-
-  return filteredPlayers;
-}
+import {
+  filteringPlayers,
+  findPlayer,
+  orderedRoles,
+  setKDAAndMedail,
+  setScoreOfPlayers,
+  sortPlayers,
+  updatingPlayerCalibrated,
+} from './functions';
 
 export function dataForApp({ players, scorePlayers, medails, roles }) {
   const playersWithScore = setScoreOfPlayers(players, scorePlayers);
@@ -104,23 +15,6 @@ export function dataForApp({ players, scorePlayers, medails, roles }) {
   const orderedPlayers = sortPlayers(filteredPlayers);
 
   return { orderedPlayers, playersWithAllData };
-}
-
-function updatingPlayerCalibrated(state, playerforUpdate) {
-  let newState = JSON.parse(JSON.stringify(state));
-  const dataForUpdate = processingPlayer(newState, playerforUpdate);
-  let indexInRanking = findPlayer(newState.ranking, dataForUpdate['_id']);
-  if (indexInRanking === -1) {
-    newState.ranking.push(dataForUpdate);
-    sortPlayers(newState.ranking);
-  } else {
-    newState.ranking[indexInRanking] = dataForUpdate;
-  }
-  let indexInAllPlayers = findPlayer(newState.allPlayers, dataForUpdate['_id']);
-
-  newState.allPlayers[indexInAllPlayers] = dataForUpdate;
-
-  return newState;
 }
 
 export function updatingPlayer(state, playerforUpdate, calibration) {
@@ -143,15 +37,4 @@ export function updatingPlayer(state, playerforUpdate, calibration) {
     default:
       break;
   }
-}
-
-function processingPlayer(state, playerWithOrderedRoles) {
-  let { partidas, kda } = calcKdaAndGames(playerWithOrderedRoles.rolesScore);
-  let playerWithMedail = setMedail(playerWithOrderedRoles, state.medails);
-
-  return { ...playerWithMedail, kda, partidas };
-}
-
-function findPlayer(arrayToSearch = [], playerId) {
-  return arrayToSearch.findIndex((player) => player['_id'] === playerId);
 }
